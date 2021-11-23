0bbc7a0f0bce5d1b4607bb84e452f9d50850e9d4
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2021-11-23 23:03:33 +0700

    Add snippet

---
+++ snippet/build/data/_template.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'group'  => '...',
+    'data' => [
+        /*
+        [
+            ['tag'],
+            'content'
+        ],
+        */
+    ]
+];
---
+++ snippet/build/data/github.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'group'  => 'Github',
+    'data' => [
+        /*
+        [
+            ['tag'],
+            'content'
+        ],
+        */
+
+        [
+            ['tips'],
+            'Filter Github issues by thumb reaction: `is:issue is:open label:enhancement sort:reactions-+1-desc`'
+        ],
+    ]
+];
