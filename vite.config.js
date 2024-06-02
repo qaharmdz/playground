aae3aba851c0535239aebce96fbc9919e1193b1c
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2024-06-02 22:24:16 +0700

    Added PWA

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { VitePWA } from 'vite-plugin-pwa';
+
+import fs from 'fs';
+import path from 'path';
 
 import CONFIG from './src/config';
 
@@ -12,6 +16,16 @@ export default defineConfig({
     outDir: './../main/allama',
     emptyOutDir: true,
   },
+  define: {
+    // usage: `const CONFIG = process.env.CONFIG;`
+    'process.env.CONFIG': JSON.stringify(CONFIG),
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, './../localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, './../localhost.pem')),
+    },
+  },
   plugins: [
     react(),
     viteStaticCopy({
@@ -21,10 +35,34 @@ export default defineConfig({
           dest: '' // copies to the root of the distribution directory
         }
       ]
-    })
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Allama',
+        short_name: 'allama',
+        description: 'Khairukum man ta\'allamal Quraana wa \'allamahu (HR. Bukhari)',
+        theme_color: '#e0f2f1',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
   ],
-  define: {
-    // usage: `const CONFIG = process.env.CONFIG;`
-    'process.env.CONFIG': JSON.stringify(CONFIG),
-  }
 })
